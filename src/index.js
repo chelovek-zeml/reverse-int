1d717de5a4be451b0517043a81daff0501da8ee0
chelovek-zeml <stranger37561@gmail.com>
2022-08-08 11:27:06 +0300

    add task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+    let arr = [];
+    let str = "";
+    while (n > 0) {
+        arr.push(n % 10);
+        n = Math.floor(n / 10);
+      }
+
+      for(let i = 0; i < arr.length; i++) {
+        str = str + arr[i];
+      }
+      return str;
 }
+
+
