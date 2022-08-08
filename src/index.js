bfebc05226a252d7b9e7be057b6811103c391f24
chelovek-zeml <stranger37561@gmail.com>
2022-08-08 13:41:22 +0300

    fix format

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
     n = Math.abs(n);
     let arr = [];
     let str = "";
     while (n > 0) {
         arr.push(n % 10);
         n = Math.floor(n / 10);
-      }
+    }
 
-      for(let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
         str = str + arr[i];
-      }
-      return str;
-}
-
-
+    }
+    return str;
+};
